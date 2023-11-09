568657b3eb1b9cdb3264919b4ebec7af685c8982
John Morris <john@zultron.com>
2023-11-09 15:22:15 -0600

    za6_moveit_config:  Fix SRDF robot mount collisions
    The robot base (i.e. whatever the robot is mounted on) may have a
    collision model that intersects the robot's base link (i.e. the link
    under `link_1`).
    
    The `MoveItConfigsBuilder` class (used in
    e.g. `launch/static_virtual_joint_tfs.launch.py`) and MoveIt Studio
    configs all need static SRDFs at run time.  To accomplish this for the
    former, render a basic SRDF into `config/za6.srdf`, and for the
    latter, provide a CMake function for generating SRDFs for the ZA6 and
    each gripper in the `za6_tools` package, plus a configurable robot
    base link.

---
+++ za6_moveit_config/za6_moveit_config-extras.cmake
@@ -0,0 +1,37 @@
+# find_package(za6_moveit_config REQUIRED)
+
+find_package(xacro REQUIRED)
+
+# List of grippers from za6_tools
+set(grippers pivot_gripper robotiq_hand_e versabuilt_multigrip none)
+
+# SRDF xacro file
+set(za6_srdf_xacro ${za6_moveit_config_DIR}/../config/za6.srdf.xacro)
+
+# Call gen_za6_gripper_srdfs("robot_base_mount") to generate SRDFs for
+# the ZA6, one for each gripper, with ZA6 base and robot_base_mount
+# collisions disabled.  With no arg, don't set robot_base_mount
+# collisions.
+function (gen_za6_gripper_srdfs)
+  list(LENGTH ARGN extra_count)
+  if (${extra_count} GREATER 1)
+    message(FATAL_ERROR "Too many arguments to gen_za6_gripper_srdfs()" )
+  endif()
+  set(robot_base_link "${ARGN}")
+
+  foreach(gripper ${grippers})
+    set(output_filename config/za6.${gripper}.srdf)
+
+    # create a rule to generate ${output_filename} from the SRDF
+    xacro_add_xacro_file(
+      ${za6_srdf_xacro} ${output_filename}
+      REMAP gripper:=${gripper} mount_link:=${robot_base_link})
+
+    list(APPEND za6_srdf_files ${XACRO_OUTPUT_FILE})
+  endforeach(gripper)
+
+  # add an abstract target to actually trigger the builds
+  add_custom_target(srdf_files ALL DEPENDS ${za6_srdf_files})
+
+  install(FILES ${za6_srdf_files} DESTINATION share/${PROJECT_NAME}/config)
+endfunction()
