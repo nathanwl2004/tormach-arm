9147e0bcbfac0e7800c9cdeae345883b4505797d
John Morris <john@zultron.com>
2023-11-10 16:52:23 -0600

    za6_hardware:  Add tests to package

--- za6_hardware/hal_plumber/drive_state.py
+++ za6_hardware/hal_plumber/drive_state.py
@@ -1,3 +1,31 @@
+# Copyright (c) 2023 Tormach, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the {copyright_holder} nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 from hal_hw_interface.ros_hal_component import RosHalComponent
 from hal_hw_interface.hal_pin_attrs import HalPinDir, HalPinType
 from .hw_device_mgr import ZADrives, ZASimDrives, ZAHWDeviceMgr
@@ -26,9 +54,7 @@ class StateError(RuntimeError):
 
 @attr.s()
 class DriveState(RosHalComponent):
-    """
-    HAL user component that enables/disables drives through ROS service.
-    """
+    """HAL user component that enables/disables drives through ROS service."""
 
     joint_error_epsilon = 0.001  # radians
 
